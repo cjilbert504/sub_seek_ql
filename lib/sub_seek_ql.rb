7b5c9bb3ca663b2a36e27075c16b03534d204f3c
Collin Jilbert <cjilbert504@gmail.com>
2021-08-24 21:43:00 -0500

    WIP: This got the method to a working state in another app

--- lib/sub_seek_ql.rb
+++ lib/sub_seek_ql.rb
@@ -3,7 +3,8 @@ require "sub_seek_ql/version"
 module SubSeekQl
   # class Error < StandardError; end
   
-  def self.last_n_records_by_foreign_key_association(foreign_key_column_name, limit)
-    self.find_by_sql("SELECT * FROM #{self.downcase.to_s += "s"} AS p1 WHERE p1.id IN (SELECT id FROM #{self.downcase.to_s += "s"} AS p2 WHERE p2.#{foreign_key_column_name.to_s} = p1.#{foreign_key_column_name.to_s} LIMIT #{limit.to_s})")
+  def self.last_n_records_by_foreign_key_association(klass, foreign_key_column_name, limit)
+    klass = klass.constantize
+    klass.find_by_sql("SELECT * FROM #{(klass.to_s + "s").downcase} AS p1 WHERE p1.id IN (SELECT id FROM #{(klass.to_s + "s").downcase} AS p2 WHERE p2.#{foreign_key_column_name.to_s} = p1.#{foreign_key_column_name.to_s} LIMIT #{limit.to_s}) ORDER BY p1.#{foreign_key_column_name.to_s}")
   end
 end
