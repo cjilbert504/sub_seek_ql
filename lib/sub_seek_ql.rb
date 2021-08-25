25a5493a83305bc5aecea5fbdc1dc5075a70803d
Collin Jilbert <cjilbert504@gmail.com>
2021-08-25 16:39:58 -0500

    Some method clean up and notes, set a default limit of 1 and set the ordering to be by created_at desc

--- lib/sub_seek_ql.rb
+++ lib/sub_seek_ql.rb
@@ -3,15 +3,10 @@ require "sub_seek_ql/version"
 module SubSeekQl
   # class Error < StandardError; end
   
-  def self.last_n_records_by_association(klass, association_column_name, limit)
-    klass_const = klass.constantize
+  def last_n_records_by_association(association_column_name, limit = 1)
 
-    # begin
-    #   klass_const.find_by_sql("SELECT * FROM #{(klass + "s").downcase} AS p1 WHERE p1.id IN (SELECT id FROM #{(klass + "s").downcase} AS p2 WHERE p2.#{association_column_name} = p1.#{association_column_name} LIMIT #{limit.to_s}) ORDER BY p1.#{association_column_name}")
-    # rescue NoMethodError
-    #   STDERR.puts "klass arg is nil"
-    # end
-
-    klass_const.find_by_sql("SELECT * FROM #{(klass + "s").downcase} AS p1 WHERE p1.id IN (SELECT id FROM #{(klass + "s").downcase} AS p2 WHERE p2.#{association_column_name} = p1.#{association_column_name} LIMIT #{limit.to_s}) ORDER BY p1.#{association_column_name}")
+    # TODO: Need to pull in a better way to pluralize class names. Initial thought is to pull in ActiveSupport and use it's pluralize helper,
+    # though maybe it's too much to pull in just for that code at the moment? Maybe just yank the pluralize method itself for now?
+    self.find_by_sql("SELECT * FROM #{(self.to_s + "s").downcase} AS p1 WHERE p1.id IN (SELECT id FROM #{(self.to_s + "s").downcase} AS p2 WHERE p2.#{association_column_name} = p1.#{association_column_name} LIMIT #{limit.to_s}) ORDER BY p1.created_at DESC")
   end
 end
