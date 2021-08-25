8d6c6cf9c716377f3b14ab17aa004047157cfed6
Collin Jilbert <cjilbert504@gmail.com>
2021-08-24 22:25:53 -0500

    place code within a rescue block

--- lib/sub_seek_ql.rb
+++ lib/sub_seek_ql.rb
@@ -5,6 +5,10 @@ module SubSeekQl
   
   def self.last_n_records_by_association(klass, association_column_name, limit)
     klass_const = klass.constantize
-    klass_const.find_by_sql("SELECT * FROM #{(klass + "s").downcase} AS p1 WHERE p1.id IN (SELECT id FROM #{(klass + "s").downcase} AS p2 WHERE p2.#{association_column_name} = p1.#{association_column_name} LIMIT #{limit.to_s}) ORDER BY p1.#{association_column_name}")
+    begin
+      klass_const.find_by_sql("SELECT * FROM #{(klass + "s").downcase} AS p1 WHERE p1.id IN (SELECT id FROM #{(klass + "s").downcase} AS p2 WHERE p2.#{association_column_name} = p1.#{association_column_name} LIMIT #{limit.to_s}) ORDER BY p1.#{association_column_name}")
+    rescue NoMethodError
+      STDERR.puts "klass arg is nil"
+    end
   end
 end
